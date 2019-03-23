3528c652f35bc5404219ebfdee6f4f08dc7dcd95
박민욱 <minwook_pro@bagmin-ug-ui-MacBookPro.local>
2019-03-24 04:40:49 +0900

    Updates

---
+++ precache-manifest.3315f8b611586ab8d612343b8b19672a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd3dbbb8fb5344004152f558623c98b3",
+    "url": "/note-app/static/media/plus.bd3dbbb8.svg"
+  },
+  {
+    "revision": "faecfcee25dbb78d30af",
+    "url": "/note-app/static/js/runtime~main.acfb4a4a.js"
+  },
+  {
+    "revision": "cefaee13749c796134b0",
+    "url": "/note-app/static/js/main.08a80323.chunk.js"
+  },
+  {
+    "revision": "f6693091a7ed0224e737",
+    "url": "/note-app/static/js/2.1af4e2df.chunk.js"
+  },
+  {
+    "revision": "711774e74fb4e41eb2e36b5ef8c765fa",
+    "url": "/note-app/index.html"
+  }
+];
